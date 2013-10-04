448a57c3c1cb03dac5f7a705e09227d360acbfc4
ceclinux <src655@gmail.com>
2013-10-05 02:12:25 +0800

    1.1 version

--- src/URLConnectionReader.java
+++ src/URLConnectionReader.java
@@ -1,49 +1,64 @@
-
 import java.net.*;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.*;
 
-
 public class URLConnectionReader {
+	private static final String GET_URL_REGEX = ".*href=\"([^\"]*)\".*";
+
 	public static void main(String[] args) throws Exception {
-		System.out.println(getURLContent(args[0], "http://www.qq.com",
-				new LinkedList<String>()));
+		String url = "http://www.amazon.com";
+		String keyword = args[0];
+		LinkedList<String> hyperLinkList = new LinkedList<String>();
+		int depth = 1;
+		System.out.println(getURLContent(keyword, url, hyperLinkList, depth));
 
 	}
 
 	public static LinkedList<String> getURLContent(String keyword, String url,
-			LinkedList<String> l) throws Exception {
-		URL oracle = new URL(url);
-		URLConnection yc = oracle.openConnection();
+			LinkedList<String> returnList, int depth) throws Exception {
+		if (depth == 0) {
+			return returnList;
+		}
+		URL searchURL = new URL(url);
+		URLConnection urlConnect = searchURL.openConnection();
 		// solve the Chinese character problems
 		BufferedReader in = new BufferedReader(new InputStreamReader(
-				yc.getInputStream(), "gb2312"));
+				urlConnect.getInputStream(), "gb2312"));
 		String inputLine;
-		StringBuilder s = new StringBuilder("");
+		StringBuilder textBuilder = new StringBuilder("");
 		while ((inputLine = in.readLine()) != null) {
-			s.append(inputLine.replaceAll(" ", ""));
+			textBuilder.append(inputLine);
 		}
-		String urlin = "";
-		String t = s.toString();
-		String r = "<a([^>]*)>([^<]*" + keyword + "[^>]*)</a>";
-		Pattern p = Pattern.compile(r);
-		Matcher m = p.matcher(t);
-		while (m.find() != false) {
-			urlin = m.group(1).replaceAll(".*href=\"([^\"]*)\".*", "$1");
+		String urlLink = "";
+		String t = textBuilder.toString();
+		String regexSearchString = "<a([^>]*)>([^<]*" + keyword + "[^>]*)</a>";
+		Pattern p = Pattern.compile(regexSearchString);
+		Matcher matcher = p.matcher(t);
+		while (matcher.find() != false) {
+			urlLink = matcher.group(1).replaceAll(GET_URL_REGEX, "$1");
+			String keySentence = matcher.group(2);
 			// System.out.println(m.group());
 			// System.out.println(m.group(2));
-			if (!l.contains(m.group(2))) {
-				l.add(m.group(2));
-				if (!urlin.equals("") && urlin.contains("qq")) {
-					getURLContent(keyword, urlin, l);
+			if (!returnList.contains(keySentence)) {
+				returnList.add(keySentence);
+				if (!urlLink.equals("") && urlLink.contains("qq")) {
+					getURLContent(keyword, urlLink, returnList, depth - 1);
 				}
 			}
 		}
+
 		in.close();
-		return l;
+
+		return returnList;
+
 	}
 
-	
+	// private static void filterRecur(String keyword,
+	// LinkedList<String> returnList, String urlLink, String keySentence)
+	// throws Exception {
+	//
+	// }
+
 }
